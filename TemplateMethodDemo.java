3f3662711d3cfcd46c1bb8cd85aa33f8c7d28a7b
Boonruksa036BL <664259036@webmail.npru.ac.th>
2025-07-14 12:05:05 +0700

    add

---
+++ TemplateMethodDemo.java
@@ -0,0 +1,9 @@
+import th.ac.npru.se.bl.Generalization;
+import th.ac.npru.se.bl.Realization;
+
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Generalization algorithm = new Realization();
+        algorithm.findSolution();
+    }
+}
